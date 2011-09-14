8cfd66022ce816c950f948f35e070180851faf4a
Syed Asad Rahman <s9asad@gmail.com>
2011-09-14 09:30:07 +0100

    added test cases examples for MCS and Substructure searches.

---
+++ test/smsd/algorithm/vflib/VF2MCSHandlerTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smsd.algorithm.vflib;
+
+import java.util.Map;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.smiles.SmilesParser;
+import smsd.AtomAtomMapping;
+
+/**
+ *
+ * @author Asad
+ */
+public class VF2MCSHandlerTest {
+
+    public VF2MCSHandlerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test ring match using MCS VF2Plus
+     * @throws Exception
+     */
+    @Test
+    public void testVF2MCS() throws Exception {
+        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        // Benzene
+        IAtomContainer query = sp.parseSmiles("C1=CC=CC=C1");
+        // Napthalene
+        IAtomContainer target = sp.parseSmiles("C1=CC2=C(C=C1)C=CC=C2");
+        //Algorithm is VF2MCS
+        //Bond Sensitive is set True
+        //Ring Match is set True
+
+        VF2MCSHandler comparison = new VF2MCSHandler();
+        comparison.set(query, target);
+        comparison.searchMCS(true, true);
+
+        Assert.assertEquals(6, comparison.getFirstAtomMapping().getCount());
+        Assert.assertEquals(24, comparison.getAllAtomMapping().size());
+
+        /**
+         * Print the mapping between molecules
+         **/
+        System.out.println(" Mappings: ");
+        for (AtomAtomMapping atomatomMapping : comparison.getAllAtomMapping()) {
+            for (Map.Entry<IAtom, IAtom> mapping : atomatomMapping.getMappings().entrySet()) {
+                IAtom sourceAtom = mapping.getKey();
+                IAtom targetAtom = mapping.getValue();
+                System.out.println(sourceAtom.getSymbol() + " " + targetAtom.getSymbol());
+                System.out.println(atomatomMapping.getQueryIndex(sourceAtom) + " " + atomatomMapping.getTargetIndex(targetAtom));
+            }
+            System.out.println("");
+        }
+    }
+}
